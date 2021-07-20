78b8877eaea546d0d759bf47d9399b0b933e05a0
Romero Gonçalves <romerotg@gmail.com>
2021-07-20 16:24:28 -0300

    Creating and styling sign-in button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
+import { SignInButton } from '../SignInButton';
+import Image from 'next/image';
 import styles from './styles.module.scss';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src="/images/logo.svg" alt="ig.news" width={110} height={31} />
         <nav>
           <a href="#" className={styles.active}>Home</a>
           <a href="#">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
