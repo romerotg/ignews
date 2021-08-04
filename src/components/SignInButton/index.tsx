aa2546c04a69ce9909e0a4da00705129bb06312a
Romero Gonçalves <romerotg@gmail.com>
2021-08-04 18:05:20 -0300

    Installing next-auth and configuring authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,10 +2,14 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
-export function SignInButton() {
-  const isUserLoggedIn = true;
+import { signIn, useSession } from 'next-auth/client'
 
-  return isUserLoggedIn ? (
+export function SignInButton() {
+  const [session] = useSession();
+
+  console.log(session);
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
@@ -18,6 +22,7 @@ export function SignInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign in with GitHub
