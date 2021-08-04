f0d93a5e6a9121c42111447778b0394f09991c8d
Romero Gonçalves <romerotg@gmail.com>
2021-08-04 18:58:23 -0300

    Fixing next-auth filename and, using session info to get the name and implementing sign-out

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,20 +2,19 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
-import { signIn, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 export function SignInButton() {
   const [session] = useSession();
-
-  console.log(session);
 
   return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Romero Gonçalves
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
